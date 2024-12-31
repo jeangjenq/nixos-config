032c181162a40131d02e7acd3d25d45ab226a3b2
jeangjenq <jeangjenq@jeangjenq.com>
2025-01-01 00:29:52 +1300

    harden ssh with authorizedKeys

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ userSettings, ... }:
 # this profile is just work + games, players... personal use stuff
 {
   imports = [
     ../work/configuration.nix
     ../../system/hardware-configuration.nix
+
+    # sshd for setting up nixos in vm
+    (import ../../system/network/sshd.nix {
+      authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlJlA2WwrobWq0eq1UKfiJd/+e7HyTp13mzl0fP/e/b jeangjenq@worf"];
+      inherit userSettings; })
   ];
 }
