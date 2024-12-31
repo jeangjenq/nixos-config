aab10f7dec4bdb52c28f9afd481a4ee2bdbf9719
jeangjenq <jeangjenq@jeangjenq.com>
2025-01-01 03:20:05 +1300

    added much more preferences

--- user/wm/gnome.nix
+++ user/wm/gnome.nix
@@ -1,10 +1,35 @@
-{ userSettings, ... }:
+{ ... }:
 
 {
-  # home-manager.users.${userSettings.username} = {
-  dconf = {
-    enable = true;
-    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  dconf.settings = {
+    
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      show-battery-percentage = true;
+    };
+
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "flat";
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/privacy" = {
+      recent-files-max-age = -1;
+      remember-recent-files = false;
+      remove-old-temp-files = true;
+      remove-old-trash-files = true;
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = true;
+    };
+
+    "org/gnome/shell/app-switcher" = {
+      current-workspace-only = true;
+    };
+  
   };
-  # };
 }
