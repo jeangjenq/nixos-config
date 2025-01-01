18614ab48f427b060b3b13b0742430833ac84d96
jeangjenq <jeangjenq@jeangjenq.com>
2025-01-01 22:05:58 +1300

    tweaked power settings, prefer longer screen time

--- user/wm/gnome.nix
+++ user/wm/gnome.nix
@@ -15,7 +15,8 @@
     "org/gnome/desktop/peripherals/touchpad" = {
       two-finger-scrolling-enabled = true;
     };
-
+    
+    # don't track my file history but auto clear temp and trash
     "org/gnome/desktop/privacy" = {
       recent-files-max-age = -1;
       remember-recent-files = false;
@@ -30,7 +31,18 @@
     "org/gnome/shell/app-switcher" = {
       current-workspace-only = true;
     };
-     
+    
+    # only turn off screen after idling 10 minutes
+    "org/gnome/desktop/session" = {
+      idle-delay = 600;
+    };
+    
+    # never auto-suspend when powered
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
+    };
+    
+    # activate appindicator extension, assuming it's installed
     "org/gnome/shell" = {
       enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" ];
     }; 
