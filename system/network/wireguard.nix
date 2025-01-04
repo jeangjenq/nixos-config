42ec24604b476421a0b31a4bda7d8f4787053a1c
jeangjenq <jeangjenq@jeangjenq.com>
2025-01-05 01:31:21 +1300

    wireguard config to use with NetworkManager

---
+++ system/network/wireguard.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+
+# we are using nmcli to import a wireguard config
+# so it's not necessary to install wireguard
+# just setting up the firewall rules to allow wireguard through
+# https://nixos.wiki/wiki/WireGuard#Setting_up_WireGuard_with_NetworkManager
+{
+  networking.firewall = {
+   # if packets are still dropped, they will show up in dmesg
+   logReversePathDrops = true;
+   # wireguard trips rpfilter up
+   extraCommands = ''
+     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+   '';
+   extraStopCommands = ''
+     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+   '';
+  };
+}
