ed6a79f6121a71480395ae66383fa6721fe08ee2
jeangjenq <jeangjenq@jeangjenq.com>
2025-01-04 13:40:25 +1300

    trying out some styling from internet

--- themes/stylix.nix
+++ themes/stylix.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   stylix.enable = true;
@@ -6,5 +6,36 @@
   stylix.polarity = "dark";
 
   stylix.targets.waybar.enable = false;
-  stylix.opacity.terminal = 0.85;
+
+  stylix.cursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
+
+  stylix.fonts = {
+    monospace = {
+      package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+      name = "JetBrainsMono Nerd Font Mono";
+    };
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+    serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serif";
+    };
+  };
+
+  stylix.fonts.sizes = {
+    applications = 10;
+    terminal = 12;
+    popups = 10;
+  };
+  
+  stylix.opacity = {
+    popups = 0.8;
+    terminal = 0.85;
+  };
 }
