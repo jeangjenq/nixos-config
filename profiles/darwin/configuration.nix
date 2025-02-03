a4cf43de84228bd6f7991dcf5af421e0321a63ed
jeangjenq <jeangjenq@jeangjenq.com>
2025-02-04 08:32:55 +1300

    dock default

--- profiles/darwin/configuration.nix
+++ profiles/darwin/configuration.nix
@@ -36,6 +36,10 @@
     };
   };
 
+  system.defaults = {
+    dock.autohide = true;
+  };
+
   # Necessary for using flakes on this system.
   nix.settings.experimental-features = "nix-command flakes";
 
