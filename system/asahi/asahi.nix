5aa80fdd315ca91f9c1dcefeb1c5e9c2a787ce4e
jeangjenq <jeangjenq@jeangjenq.com>
2025-02-12 08:53:03 +1300

    wireless service and env var for gtk apps

--- system/asahi/asahi.nix
+++ system/asahi/asahi.nix
@@ -6,10 +6,10 @@
     inputs.nixos-apple-silicon.nixosModules.apple-silicon-support
   ];
 
-  nixpkgs.overlays = [
-    inputs.nixos-apple-silicon.overlays.apple-silicon-overlay
-    (final: prev: { mesa = final.mesa-asahi-edge; })
-  ];
+  # nixpkgs.overlays = [
+  #   inputs.nixos-apple-silicon.overlays.apple-silicon-overlay
+  #   (final: prev: { mesa = final.mesa-asahi-edge; })
+  # ];
 
   hardware = {
     asahi = {
@@ -20,5 +20,15 @@
     graphics.enable = true;
   };
 
+  networking.wireless.iwd = {
+    enable = true;
+    settings.General.EnableNetworkConfiguration = true;
+  };
+
   boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
+
+  # fix an issue where gtk applications can't detect wayland display
+  environment.sessionVariables = {
+    GSK_RENDERER="gl";
+  };
 }
