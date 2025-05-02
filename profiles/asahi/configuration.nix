25858affa75eddb562bc665fdeacb4f97807d127
jeangjenq <jeangjenq@jeangjenq.com>
2025-05-02 15:56:51 +1200

    clear out config faster to save space

--- profiles/asahi/configuration.nix
+++ profiles/asahi/configuration.nix
@@ -25,6 +25,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 2;
   boot.loader.efi.canTouchEfiVariables = false;
 
   # Enable networking
