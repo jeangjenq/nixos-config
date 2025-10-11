bbfde3a898522a4304dd2b2def7fe385165904f6
jeangjenq <jeangjenq@jeangjenq.com>
2025-10-12 09:48:25 +1300

    frankly something cutting edge need to stay stable on my system

--- system/app/llm.nix
+++ system/app/llm.nix
@@ -1,15 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-stable, ... }:
 
 {
   services = {
     ollama = {
       enable = true;
       acceleration = "rocm";
+      package = pkgs-stable.ollama;
     };
 
     open-webui = {
       enable = true;
       port = 4173;
+      package = pkgs-stable.open-webui;
     };
   };
 }
