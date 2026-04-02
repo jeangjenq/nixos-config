3e517287e403942ce3192ac830b8185e3dba1283
jeangjenq <jeangjenq@jeangjenq.com>
2026-04-02 14:33:37 +1300

    creating a minimal profile

---
+++ profiles/minimal/configuration.nix
@@ -0,0 +1,93 @@
+{ config, pkgs, systemSettings, userSettings, ... }:
+
+{
+  imports =
+    [ # hardwares
+      ../../system/hardware-configuration.nix
+      ../../system/hardware/opengl.nix
+      ../../system/hardware/bluetooth.nix
+      ../../system/hardware/automount.nix
+      ../../system/network/wireguard.nix
+      
+      ( ./. + "../../../system/wm" + ("/" + systemSettings.wm) + ".nix" )
+      
+      ../../system/virtualization/virtualization.nix
+
+      # sshd for setting up nixos in vm
+      (import ../../system/network/sshd.nix {
+        authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEQnJVdL98B5voLeFHF9pGhNBW6mudDPJM2By159a/6 jeangjenq@worf"];
+        inherit userSettings; })
+    ];
+
+  # personal preferences on powerkey and suspend behaviour
+  services.logind.settings.Login = {
+    HandlePowerKey = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Bootloader.
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/sda";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = systemSettings.hostname; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = systemSettings.timezone;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = systemSettings.locale;
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = systemSettings.locale;
+    LC_IDENTIFICATION = systemSettings.locale;
+    LC_MEASUREMENT = systemSettings.locale;
+    LC_MONETARY = systemSettings.locale;
+    LC_NAME = systemSettings.locale;
+    LC_NUMERIC = systemSettings.locale;
+    LC_PAPER = systemSettings.locale;
+    LC_TELEPHONE = systemSettings.locale;
+    LC_TIME = systemSettings.locale;
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${userSettings.username} = {
+    isNormalUser = true;
+    description = userSettings.username;
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = [];
+    uid = 1000;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
+    wget
+    git
+    home-manager
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  # enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+}
---
+++ profiles/minimal/home.nix
@@ -0,0 +1,42 @@
+{ pkgs, pkgs-stable, systemSettings, userSettings, ... }:
+
+{
+
+  imports = [
+    ( ./. + "../../../user/wm" + ( "/" + systemSettings.wm + ".nix") )
+    (./. + "../../../user/shell" + ("/" + userSettings.term) + ".nix")
+    ../../user/app/git/git.nix
+    ../../user/shell/sh.nix
+    ../../user/shell/yazi.nix
+    ../../user/shell/helix.nix
+    ../../user/virtualization/virtualization.nix
+    ../../user/app/browser/firefox.nix
+    ../../themes/stylix.nix
+  ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/" + userSettings.username;
+
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+  programs.home-manager.enable = true;
+
+  programs.firefox.enable = true;
+  
+  home.packages = (with pkgs; [
+    # core
+    adwaita-icon-theme
+    mission-center
+
+  ])
+  ++
+  (with pkgs-stable ;[
+  ]);
+
+  home.sessionVariables = {
+  };
+
+  xdg.configFile."mimeapps.list".force = true;
+
+}
