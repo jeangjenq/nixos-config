11a3e4c41610544cd297d7d2b97371a3b8cf428a
jeangjenq <jeangjenq@jeangjenq.com>
2026-05-05 16:59:43 +1200

    avoid 1.6.3 for now to avoid ladspa change

--- system/wm/pipewire.nix
+++ system/wm/pipewire.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{ pkgs-stable, ... }:
 
 {
   # Pipewire
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    package = pkgs-stable.pipewire;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
