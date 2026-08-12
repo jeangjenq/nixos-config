0262928909b798776382cc61b246c73c780e23de
jeangjenq <jeangjenq@jeangjenq.com>
2026-08-13 10:58:12 +1200

    startup can get cram in nix

--- user/wm/hyprland.nix
+++ user/wm/hyprland.nix
@@ -47,7 +47,24 @@ in
         autoLoad = true;
       };
       "startups" = {
-        content = ./hyprland/startups.lua;
+        content = ''
+          hl.on("hyprland.start", function()
+            -- essentials
+            hl.exec_cmd("nm-applet --indicator")
+            hl.exec_cmd("blueman-applet")
+            hl.exec_cmd("protonmail-bridge --noninteractive")
+            hl.exec_cmd("nextcloud")
+            hl.exec_cmd("systemctl --user start hyprpolkitagent")
+
+            -- preferences
+            hl.exec_cmd("[workspace 1] firefox")
+            hl.exec_cmd("[workspace 5 silent] MANGOHUD=1 steam")
+            hl.exec_cmd("[workspace 6 silent] discord")
+            hl.exec_cmd("[workspace 7 silent] signal-desktop")
+            hl.exec_cmd("[workspace 8 silent] sleep 10 && thunderbird")
+            hl.exec_cmd("[workspace 9 silent] feishin")
+          end)
+        '';
         autoLoad = true;
       };
     };
