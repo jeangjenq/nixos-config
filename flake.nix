b9b3cc383a2bccb5b425ef389626bcc7b5568a04
jeangjenq <jeangjenq@jeangjenq.com>
2026-08-12 20:55:26 +1200

    stable ready to move on

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "nixpkgs/nixos-26.05";
     stylix.url = "github:danth/stylix";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager"; 
